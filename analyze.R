dd6e46d9f399ccf61ad8885800636709b9af6abf
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-04-08 19:24:35 -0400

    Split out negative controls. Tells same story so leaving plot as is

--- analyze.R
+++ analyze.R
@@ -9,6 +9,9 @@ mobioNegs<-na.omit(unlist(mobio[,c('OR.Air.Swab')]))
 mobioPlacenta<-na.omit(unlist(mobio[,c('Placenta..MS.','Placenta..FS.')]))
 mobioSaliva<-na.omit(unlist(mobio[,c('Saliva')]))
 mobioOtherNegs<-na.omit(unlist(mobio[,c('Sterile.Swab','Water','Extraction.Blank')]))
+mobioSwab<-na.omit(unlist(mobio[,c('Sterile.Swab')]))
+mobioBlank<-na.omit(unlist(mobio[,c('Extraction.Blank')]))
+mobioWater<-na.omit(unlist(mobio[,c('Water')])) #only 1 samples
 
 #pspNegs<-na.omit(unlist(psp[,c('OR.Air.Swab','Sterile.Swab','Water','Extraction.Blank')]))
 pspNegs<-na.omit(unlist(psp[,c('OR.Air.Swab')]))
@@ -31,25 +34,34 @@ pspSalivaDiff<-pspSaliva$mu1-pspSaliva$mu2
 mobioSalivaDiff<-mobioSaliva$mu1-mobioSaliva$mu2
 
 mobioOther<-cacheOperation('otherMobioBayes.Rdat',BESTmcmc,mobioNegs,mobioOtherNegs,burnInSteps=2e5,thinSteps=10,numSavedSteps=2e6,parallel=TRUE)
+mobioSwabBest<-cacheOperation('swabMobioBayes.Rdat',BESTmcmc,mobioNegs,mobioSwab,burnInSteps=2e5,thinSteps=10,numSavedSteps=2e6,parallel=TRUE)
+mobioBlankBest<-cacheOperation('blankMobioBayes.Rdat',BESTmcmc,mobioNegs,mobioBlank,burnInSteps=2e5,thinSteps=10,numSavedSteps=2e6,parallel=TRUE)
+#mobioWaterBest<-cacheOperation('waterMobioBayes.Rdat',BESTmcmc,mobioNegs,mobioWater,burnInSteps=2e5,thinSteps=10,numSavedSteps=2e6,parallel=TRUE)
 pspOther<-cacheOperation('otherPspBayes.Rdat',BESTmcmc,pspNegs,pspOtherNegs,burnInSteps=2e5,thinSteps=10,numSavedSteps=2e6,parallel=TRUE)
 pspOtherDiff<-pspOther$mu1-pspOther$mu2
 mobioOtherDiff<-mobioOther$mu1-mobioOther$mu2
+mobioSwabDiff<-mobioSwabBest$mu1-mobioSwabBest$mu2
+mobioBlankDiff<-mobioBlankBest$mu1-mobioBlankBest$mu2
+#mobioWaterDiff<-mobioWaterBest$mu1-mobioWaterBest$mu2
 
 
+
+
+xlim<-c(-5,25)
+#prettyLabels<-pretty(c(-5,25))
+prettyLabels<-pretty(log10(2^c(-5,25)))
+bins<-seq(xlim[1],xlim[2],.1)
 pspTab<-table(cut(pspDiff,bins))/length(pspDiff)
 mobioTab<-table(cut(mobioDiff,bins))/length(mobioDiff)
 pspSalivaTab<-table(cut(pspSalivaDiff,bins))/length(pspSalivaDiff)
 mobioSalivaTab<-table(cut(mobioSalivaDiff,bins))/length(mobioSalivaDiff)
 pspOtherTab<-table(cut(pspOtherDiff,bins))/length(pspOtherDiff)
 mobioOtherTab<-table(cut(mobioOtherDiff,bins))/length(mobioOtherDiff)
-
+mobioSwabTab<-table(cut(mobioSwabDiff,bins))/length(mobioSwabDiff)
+mobioBlankTab<-table(cut(mobioBlankDiff,bins))/length(mobioBlankDiff)
 
 pdf('mobioPsp.pdf')
 par(mfrow=c(2,1),las=1,mar=c(4,3.6,1.1,.1))
-xlim<-c(-5,25)
-#prettyLabels<-pretty(c(-5,25))
-prettyLabels<-pretty(log10(2^c(-5,25)))
-bins<-seq(xlim[1],xlim[2],.1)
 plot(1,1,type='n',xlim=xlim,ylim=range(pspTab,pspSalivaTab,pspOtherTab),xlab='',xaxt='n',ylab='Posterior probability',mgp=c(2.7,.8,0),main='PSP')
 #axis(1,prettyLabels,sapply(prettyLabels,function(x)as.expression(bquote(2^.(x)))),las=1)
 axis(1,log2(10^prettyLabels),ifelse(prettyLabels==0,1,sapply(prettyLabels,function(x)as.expression(bquote(10^.(x))))),las=1)
@@ -66,6 +78,8 @@ title(xlab='Fold increase in DNA over air swabs',mgp=c(2,1,0))
 polygon(c(-5,bins[-length(bins)]+.05,25,-5),c(0,unlist(mobioTab),0,0),col='#0000FF44')
 polygon(c(-5,bins[-length(bins)]+.05,25,-5),c(0,unlist(mobioSalivaTab),0,0),col='#FF000044')
 polygon(c(-5,bins[-length(bins)]+.05,25,-5),c(0,unlist(mobioOtherTab),0,0),col='#00FF0044')
+#polygon(c(-5,bins[-length(bins)]+.05,25,-5),c(0,unlist(mobioSwabTab),0,0),col='#00FF0044')
+#polygon(c(-5,bins[-length(bins)]+.05,25,-5),c(0,unlist(mobioBlankTab),0,0),col='#00FF0044')
 abline(v=0,lty=2)
 legend('topright',c('Placenta','Saliva','Negatives'),fill=c('#0000FF44','#FF000044','#00FF0044'),bty='n')
 dev.off()
